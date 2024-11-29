b85b18dd87a427df9cd040ccfdaadd2d8a77aab6
TharunBIT <Tharunvigneshwarkk@bitsathy.ac.in>
2024-11-29 06:35:57 +0000

    BugFix in Credit Report Page Error Validation

--- controllers/credit-receipt-controller.js
+++ controllers/credit-receipt-controller.js
@@ -8,52 +8,34 @@ const cashFlowController = require("../controllers/cash-flow-controller");
 
 module.exports = {
 
-    // saveReceipts: (req, res) => {
-    //     CreditReceiptsDao.create(dbMapping.newReceipt(req));
-    //     res.redirect('/creditreceipts?receipts_fromDate=' + req.body.receipts_fromDate_hiddenValue +
-    //         '&receipts_toDate=' + req.body.receipts_toDate_hiddenValue);
-    // },
     // Create credit receipt - one at a time
     saveReceipts: (req, res) => {
         const txnReceiptDate = req.body.txn_receipt_date_0;
         let locationCode = req.user.location_code;
-        
+
         cashFlowController.checkCashFlowClosingStatus(locationCode, txnReceiptDate)
             .then(cashFlowClosing => {
-                console.log('Cash flow closing status:', cashFlowClosing);
-                console.log('Cash flow closing status:', cashFlowClosing[0].dataValues.closing_status);
-                if (cashFlowClosing[0].dataValues.closing_status === 'CLOSED') {
-                    req.flash('error', 'Cannot Create Receipt For ' + dateFormat(txnReceiptDate, 'dd-mm-yyyy') + ': Cash flow for the date is closed.');
+                if (cashFlowClosing && cashFlowClosing.length > 0 && cashFlowClosing[0].dataValues.closing_status === 'CLOSED') {
+                    console.log('Cannot create receipt: Cash flow for the date is closed.');
+                    req.flash('error', 'Access denied: Cash flow for ' + dateFormat(txnReceiptDate, 'dd-mm-yyyy') + ' is already closed');
                     res.redirect('/creditreceipts?receipts_fromDate=' + req.body.receipts_fromDate_hiddenValue +
                         '&receipts_toDate=' + req.body.receipts_toDate_hiddenValue);
-                } else if (cashFlowClosing == null || cashFlowClosing[0].dataValues.closing_status === 'DRAFT') {
+                } else {
                     CreditReceiptsDao.create(dbMapping.newReceipt(req))
                         .then(data => {
-                            if (data == 1 || data == 0) {
-                                req.flash('success', 'Saved receipt data successfully or no update needed.');
+                            console.log('CreditReceiptsDao.create result:', data);
+                            if (data) {
+                                req.flash('success', 'Saved receipt data successfully.');
                                 res.redirect('/creditreceipts?receipts_fromDate=' + req.body.receipts_fromDate_hiddenValue +
                                     '&receipts_toDate=' + req.body.receipts_toDate_hiddenValue);
-                            } 
-                            else {
-                                // req.flash('error', 'Saved receipt data failed.');
+                            } else {
+                                req.flash('error', 'Saved receipt data failed.');
                                 res.redirect('/creditreceipts?receipts_fromDate=' + req.body.receipts_fromDate_hiddenValue +
                                     '&receipts_toDate=' + req.body.receipts_toDate_hiddenValue);
                             }
                         })
-                        .catch(err => {
-                            console.error('Error saving credit receipt:', err);
-                            // req.flash('error', 'Failed to create credit receipt.');
-                            res.redirect('/creditreceipts?receipts_fromDate=' + req.body.receipts_fromDate_hiddenValue +
-                                '&receipts_toDate=' + req.body.receipts_toDate_hiddenValue);
-                        });
                 }
             })
-            .catch(err => {
-                console.error('Error checking cash flow closing status:', err);
-                // req.flash('error', 'Failed to create credit receipt.');
-                res.redirect('/creditreceipts?receipts_fromDate=' + req.body.receipts_fromDate_hiddenValue +
-                    '&receipts_toDate=' + req.body.receipts_toDate_hiddenValue);
-            });
     },
 
     // Get credit receipts - from and to date provided
@@ -61,10 +43,10 @@ module.exports = {
         let locationCode = req.user.location_code;
         let fromDate = dateFormat(new Date(), "yyyy-mm-dd");
         let toDate = dateFormat(new Date(), "yyyy-mm-dd");
-        if(req.query.receipts_fromDate) {
+        if (req.query.receipts_fromDate) {
             fromDate = req.query.receipts_fromDate;
         }
-        if(req.query.receipts_toDate) {
+        if (req.query.receipts_toDate) {
             toDate = req.query.receipts_toDate;
         }
         let receipts = [];
@@ -75,7 +57,7 @@ module.exports = {
                 values[0].value.forEach((receipt) => {
                     let creditlist = receipt.m_credit_list;
                     let isEditOrDeleteAllowed = false;
-                    if(creditlist) {
+                    if (creditlist) {
                         isEditOrDeleteAllowed = utils.noOfDaysDifference(receipt.receipt_date, utils.currentDate())
                             < config.APP_CONFIGS.receiptEditOrDeleteAllowedDays ? true : false;
                         receipts.push({
@@ -90,7 +72,8 @@ module.exports = {
                         console.warn("Server app error: Looks like data is corrupted, debug more.")
                     }
                 });
-                res.render('credit-receipts', {title: 'Credit Receipts', user: req.user,
+                res.render('credit-receipts', {
+                    title: 'Credit Receipts', user: req.user,
                     config: config.APP_CONFIGS,
                     cashReceipts: receipts,
                     creditCompanyValues: values[1].value,
@@ -98,12 +81,13 @@ module.exports = {
                     currentDate: utils.currentDate(),
                     minDateForNewReceipts: utils.restrictToPastDate(config.APP_CONFIGS.receiptEditOrDeleteAllowedDays),
                     fromDate: fromDate,
-                    toDate: toDate});
+                    toDate: toDate
+                });
             });
     },
 
     // Update credit receipt
-    updateReceipts: (req, res)  => {
+    updateReceipts: (req, res) => {
         CreditReceiptsDao.update({
             treceipt_id: req.params.id,
             receipt_type: req.body.receipt_type,
@@ -111,27 +95,27 @@ module.exports = {
             amount: req.body.amount,
             notes: req.body.notes
         }).then(data => {
-            if(data == 1 || data == 0) {
-                res.status(200).send({message: 'Saved receipt data successfully or no update needed.'});
+            if (data == 1 || data == 0) {
+                res.status(200).send({ message: 'Saved receipt data successfully or no update needed.' });
             } else {
-                res.status(500).send({error: 'Saved receipt data failed.'});
+                res.status(500).send({ error: 'Saved receipt data failed.' });
             }
         });
     },
 
     // Delete credit receipt
     deleteReceipts: (req, res, next) => {
-        if(req.query.id) {
+        if (req.query.id) {
             CreditReceiptsDao.delete(req.query.id)
                 .then(data => {
                     if (data == 1) {
-                        res.status(200).send({message: 'Receipt successfully deleted.'});
+                        res.status(200).send({ message: 'Receipt successfully deleted.' });
                     } else {
-                        res.status(500).send({error: 'Receipt deletion failed or not available to delete.'});
+                        res.status(500).send({ error: 'Receipt deletion failed or not available to delete.' });
                     }
                 });
         } else {
-            res.status(500).send({error: 'Receipt deletion failed or not available to delete.'});
+            res.status(500).send({ error: 'Receipt deletion failed or not available to delete.' });
         }
     }
 }
