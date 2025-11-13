28ff9840a4a4a3b18fa2d4b60fffaaf8f92b00e2
= <=>
2025-11-13 21:15:05 +0530

    BanK master update and Credit Master Update.

--- db/bankaccount.js
+++ db/bankaccount.js
@@ -5,24 +5,86 @@ module.exports = function(sequelize, DataTypes) {
     var Account = sequelize.define(config.BANK_TABLE, {
         bank_id: {
             type: DataTypes.INTEGER,
-            primaryKey: true
+            primaryKey: true,
+            autoIncrement: true
         },
-        bank_name: DataTypes.STRING,
-        bank_branch: DataTypes.STRING,
-        account_number: DataTypes.STRING,
-        ifsc_code: DataTypes.STRING,
-        location_id: DataTypes.INTEGER,
-        type: DataTypes.STRING,
-        cc_limit: DataTypes.INTEGER,
-        ledger_name: DataTypes.STRING,
-        account_nickname: DataTypes.STRING,
-        created_by: DataTypes.STRING,
-        updated_by: DataTypes.STRING,
-        creation_date: DataTypes.DATE,
-        updation_date: DataTypes.DATE
+        bank_name: {
+            type: DataTypes.STRING(150),
+            allowNull: true
+        },
+        bank_branch: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        account_number: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        ifsc_code: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        location_code: {
+            type: DataTypes.STRING(50),
+            allowNull: true
+        },
+        type: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        cc_limit: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        ledger_name: {
+            type: DataTypes.STRING(250),
+            allowNull: true
+        },
+        account_nickname: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        location_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        internal_flag: {
+            type: DataTypes.ENUM('Y', 'N'),
+            allowNull: true,
+            defaultValue: 'N'
+        },
+        is_oil_company: {
+            type: DataTypes.ENUM('Y', 'N'),
+            allowNull: true,
+            defaultValue: 'N'
+        },
+        active_flag: {
+            type: DataTypes.ENUM('Y', 'N'),
+            allowNull: true,
+            defaultValue: 'Y'
+        },
+        created_by: {
+            type: DataTypes.STRING(45),
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.STRING(45),
+            allowNull: true
+        },
+        creation_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW
+        },
+        updation_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW
+        }
     }, {
         timestamps: false,
         freezeTableName: true
     });
+    
     return Account;
 };
